2850c2d35f5790d20d0d53b3885f30459bd2079e
tomasherrera1910 <tomymza10@gmail.com>
2022-11-27 19:34:04 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express()
-app.use(express.json())
+app.use(express.json()) //bodyparser
 
 app.get('/status', (req, res) => {
     res.json({"status": 'ok'})
