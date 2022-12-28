f67dd8d71ffca403e4ab5177d59578e688e0a3a7
tomasherrera1910 <tomymza10@gmail.com>
2022-12-28 17:28:20 -0300

    middleware for auth

---
+++ src/middlewares/verifyUser.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt, { Secret } from 'jsonwebtoken'
+
+const verifyUser = (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
+
+    if (token === undefined) {
+      throw new Error()
+    }
+    const secret: Secret = process.env.JWT_SECRET ?? ''
+    const decodedToken = jwt.verify(token, secret)
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!decodedToken) {
+      throw new Error()
+    }
+    next()
+  } catch (_error) {
+    res.status(401).json({ error: 'Token inválido o inexistente' })
+  }
+}
+
+export default verifyUser
