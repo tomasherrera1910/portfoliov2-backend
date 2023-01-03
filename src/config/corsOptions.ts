59784d8ebacd71fb151beecd8a7b4205d634ca5e
tomasherrera1910 <tomymza10@gmail.com>
2023-01-03 19:01:25 -0300

    Adding cors options for allow cookies

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,11 +1,14 @@
-// const whitelist = ['http://localhost:3000', 'http://localhost:3001', '']
-const corsOptions = {
-  // origin: function (origin: any, callback: any) {
-  //   if (whitelist.includes(origin)) {
-  //     callback(null, true)
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'))
-  //   }
-  // }
+import { CorsOptions } from 'cors'
+
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3001/**']
+const corsOptions: CorsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
 }
 export default corsOptions
