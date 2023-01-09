ce97be7bfc943cdce986f9446c6c24953073e12e
tomasherrera1910 <tomymza10@gmail.com>
2023-01-09 13:20:39 -0300

    Adding env variable for HOST of postgres db deployed in railway

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Sequelize } from 'sequelize'
 
-const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT } = process.env
+const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_HOST } = process.env
 
 const sequelize = new Sequelize(DATABASE_NAME!, DATABASE_USER!, DATABASE_PASSWORD, {
   port: Number(DATABASE_PORT)!,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  host: DATABASE_HOST
 })
 
 export default sequelize
