cfc918013f05be3615fe99dad63253e7fa7d8ad5
tomasherrera1910 <tomymza10@gmail.com>
2023-01-09 13:28:32 -0300

    building last changes before the deploy

--- build/database/index.js
+++ build/database/index.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 const sequelize_1 = require("sequelize");
-const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT } = process.env;
+const { DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_HOST } = process.env;
 const sequelize = new sequelize_1.Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
     port: Number(DATABASE_PORT),
-    dialect: 'postgres'
+    dialect: 'postgres',
+    host: DATABASE_HOST
 });
 exports.default = sequelize;
